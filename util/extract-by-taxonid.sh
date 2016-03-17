36822153a25e59ae5f7562482ac39c684f2c2128
Zebulun Arendsee <zbwrnz@gmail.com>
2016-03-17 17:33:55 -0500

    Add code to extract species from metadata table

--- util/extract-by-taxonid.sh
+++ util/extract-by-taxonid.sh
@@ -1,15 +1,42 @@
 #!/bin/bash
 
-datfile=data/rnaseq/RNA-seq_sra.tab
+usage (){
+    echo "Extract species-specific metadata tables from full table"
+    echo "USAGE: $0    <metadata-file> <taxonids>"
+    echo "USAGE: $0 -n <metadata-file> <scientific_names>"
+    echo "USAGE: $0 -r <metadata-file> <patterns>"
+    exit 0
+}
 
-arabidopsis="Arabidopsis thaliana"
-yeast="Saccharomyces cerevisiae"
-maize="Zea mays"
+[[ $# -eq 0 ]] && usage
 
-for species in "$arabidopsis" "$yeast" "$maize"
+by_name= by_expression=
+while getopts "hnr" opt; do
+    case $opt in
+        h)
+            usage ;;
+        n)
+            by_name=1
+            shift ;;
+        r)
+            by_expression=1
+            shift ;;
+    esac 
+done
+
+datfile=$1
+shift
+
+for species in $@
 do
-    awk -v FS="\t" -v s="$species" \
-        '$8 == s {print $2, $8}' \
-        "$datfile" |
-    sort -u
+    out=${species}.tab
+    echo "Extracting $species" >&2
+    head -1 $datfile > "$out"
+    if [[ $by_name -eq 1 ]]; then
+        awk -v s="$species" 'BEGIN{FS="\t"; OFS=FS} $14 == s' $datfile >> "$out"
+    elif [[ $by_expression -eq 1 ]]; then
+        awk -v s="$species" 'BEGIN{FS="\t"; OFS=FS} $14 ~ s' $datfile >> "$out"
+    else
+        awk -v s="$species" 'BEGIN{FS="\t"; OFS=FS} $13 == s' $datfile >> "$out"
+    fi
 done
