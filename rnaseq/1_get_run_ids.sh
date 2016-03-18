b58975e66b6311e0ee4929fa78cefd72eb4f65d8
Zebulun Arendsee <arendsee@iastate.edu>
2016-03-18 00:48:26 -0500

    Add script to convert sample ids to run ids

--- rnaseq/1_get_run_ids.sh
+++ rnaseq/1_get_run_ids.sh
@@ -1,37 +1,32 @@
 #!/bin/bash
-
-# =============================================================================
-# EXAMPLE:
-# $ sra2srr SRA169702
-# SRR1363422
-# SRR1363423
-# SRR1363420
-# SRR1363421
-# SRR1363419
-# SRR1363418
-# SRR1363417
-# SRR1363416
-# SRR1363415
-# SRR1363414
-# SRR1363413
-# SRR1363412
-#
-# NOTE: This also maps ERA to ERR
-#
-# DEPENDENCIES:
-#  * xmlstarlet
-#  * wget
-#
-# AUTHOR:
-#  Zebulun Arendsee (zbwrnz@gmail.com; github.com/zbwrnz)
-# =============================================================================
-
 set -u
+
+usage (){
+cat << EOF
+Read file of SRA sample_ids (SRS*), output run_ids (SRR*)
+Required Arguments
+  -i SAMPLES file of sample ids
+EOF
+exit 0
+}
+
+[[ $# -eq 0 ]] && usage
+
+while getopts "hi:o:" opt; do
+    case $opt in
+        h)
+            usage ;;
+        i) 
+            ids=$OPTARG ;;
+        o)
+            output=$OPTARG ;;
+    esac 
+done
 
 eusrc() {
     db=$1
     term=$2
-    retmax=${3-500}
+    retmax=${3-1000}
     src_url="http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi"
     url="$src_url?db=$db&term=$term&retmax=$retmax&usehistory=y"
     out=$(wget -qO /dev/stdout $url | 
@@ -55,12 +50,15 @@ eufet() {
     done
 }
 
-for sra in $@
+mkdir -p samples
+
+while read id
 do
-    eusrc sra $sra | eufet |
+    eusrc sra $id | eufet |
+        xmlstarlet fo | tee samples/${id}.xml |
         xmlstarlet sel --template \
             --match 'EXPERIMENT_PACKAGE_SET/EXPERIMENT_PACKAGE/RUN_SET/RUN/IDENTIFIERS' \
-                --output "${sra}" --output $'\t' \
                 --value-of 'PRIMARY_ID' --nl \
             --break
-done
+    sleep 1 # so they don't block my ip address
+done < "$ids" | sort -u > "$output"
